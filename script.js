3b3c69c68cb738651e36381f2d11f741a9be306c
Matias Arias <matiasarias384@gmail.com>
2022-07-18 22:59:47 -0300

    Primera version no funcional

---
+++ script.js
@@ -0,0 +1,44 @@
+
+
+function time(value) {
+    var progressBar = document.getElementById('progressbar')
+    progressBar.style.width = `${value}%`;
+    console.log(value)
+}
+function delay(n) {
+    return new Promise(function (resolve) {
+        setTimeout(resolve, n * 1000);
+    });
+}
+async function startTimer() {
+    value = 100
+    while (value >= 0) {
+        time(value);
+        value -= 10;
+        await delay(1);
+    };
+}
+function startLyrics(){
+
+}
+function readSessions(){
+    leerArchivo();
+    document.querySelector('#sessions')
+      .addEventListener('change', leerArchivo, false)
+}
+function leerArchivo(texto, separador=';',omitirEncabezado=false) {
+    if(typeof texto !== 'string'){
+        throw TypeError('El argumento debe ser una cadena de caracteres')
+    }
+    texto.slice(omitirEncabezado ? texto.indexOf('\n')+1 : 0)
+    .split('\n')
+    .map(l => l.split(separador));
+  }
+try {
+    console.log(leerCsv('id,nombre'))
+} catch (e) {
+    console.log(`Error: ${e.message}`)
+}
+readSessions();
+startLyrics();
+startTimer();
