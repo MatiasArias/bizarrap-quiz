8b983052d5b926204e797418401abefab45ff8d9
Matias Arias <matiasarias384@gmail.com>
2022-07-18 23:42:20 -0300

    Version timer funcional

--- script.js
+++ script.js
@@ -18,28 +18,6 @@ async function startTimer() {
         await delay(1);
     };
 }
-function startLyrics(){
 
-}
-function readSessions(){
-    leerArchivo();
-    document.querySelector('#sessions')
-      .addEventListener('change', leerArchivo, false)
-}
-function leerArchivo(texto, separador=';',omitirEncabezado=false) {
-    if(typeof texto !== 'string'){
-        throw TypeError('El argumento debe ser una cadena de caracteres')
-    }
-    texto.slice(omitirEncabezado ? texto.indexOf('\n')+1 : 0)
-    .split('\n')
-    .map(l => l.split(separador));
-  }
-try {
-    console.log(leerCsv('id;nombre\n1001;Oliva\n1002;Julio'));
-} catch (e) {
-    console.log(`Error: ${e.message}`)
-}
-readSessions();
-startLyrics();
 startTimer();
 
